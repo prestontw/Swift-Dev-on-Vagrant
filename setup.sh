5c79f4d008c936b327e93510ba4e7e75e24907de
preston <tunpm-16@rhodes.edu>
2016-03-23 01:19:26 -0400

    try to use docker commit to save state of vimmed dev_base
    this didn't work.
    kept on getting an authentication error,
    so moving back to setup installing vim and such.
    We could move all of that to the dockerfile,
    as it is likely to not change between containers...

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo test > test.txt
-sudo apt-get install -y make vim git clang
+echo "starting installation of base apps"
+sudo apt-get install -y make vim git
 
 # set up minimal vim configuration
 git clone https://github.com/prestontw/dotfiles
 mv /home/vagrant/dotfiles/.vinimal /home/vagrant/.vimrc
-echo done setting up vim
+echo "done setting up vim"
 
 # script to set up swift on vagrant
+#sudo apt-get install -y clang
 #curl -O https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12....tar.gz
 #tar zxf file
 #profile="/home/vagrant/.bashrc"
