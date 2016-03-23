53139b9358ef0d571e23651080720eef5dc187bc
preston <tunpm-16@rhodes.edu>
2016-03-23 01:12:06 -0400

    comment out swift commands from setup.sh to make vimmed_base container
    will see if we can remove the other part from setup and see if we still have vim, etc

--- setup.sh
+++ setup.sh
@@ -9,10 +9,10 @@ mv /home/vagrant/dotfiles/.vinimal /home/vagrant/.vimrc
 echo done setting up vim
 
 # script to set up swift on vagrant
-curl -O https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12....tar.gz
-tar zxf file
-profile="/home/vagrant/.bashrc"
-echo "export PATH=$PATH:~/usr/local/gupc/bin" >> $profile
-source $profile
+#curl -O https://swift.org/builds/ubuntu1404/swift-2.2-SNAPSHOT-2015-12....tar.gz
+#tar zxf file
+#profile="/home/vagrant/.bashrc"
+#echo "export PATH=$PATH:~/usr/local/gupc/bin" >> $profile
+#source $profile
 
 echo done with setup!
