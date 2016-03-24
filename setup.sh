a37acd220e685a62677da92e73a263bc3d2ee3ea
preston <tunpm-16@rhodes.edu>
2016-03-24 01:04:45 -0400

    add libxml2 as a dep of swift
    and add install location of swift to path

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ mv /home/vagrant/dotfiles/.vinimal /home/vagrant/.vimrc
 echo "done setting up vim"
 
 # script to set up swift on vagrant
-sudo apt-get install -y clang libicu-dev
+sudo apt-get install -y clang libicu-dev libxml2
 curl -O https://swift.org/builds/swift-2.2-release/ubuntu1404/swift-2.2-RELEASE/swift-2.2-RELEASE-ubuntu14.04.tar.gz
 # skipping verifying gpg, etc
 tar xzf swift-2.2-RELEASE-ubuntu14.04.tar.gz
-#profile="/home/vagrant/.bashrc"
-#echo "export PATH=$PATH:~/usr/local/gupc/bin" >> $profile
-#source $profile
+profile="/home/vagrant/.bashrc"
+echo "export PATH=$PATH:~/swift-2.2-RELEASE-ubuntu14.04/usr/bin" >> $profile
+source $profile
 
 echo done with setup!
